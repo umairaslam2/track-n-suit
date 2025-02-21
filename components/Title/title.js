3a0a764489edd3738f13f677aed431d9c2bd20d6
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2025-02-21 12:36:40 +0500

    fix: adjust title and subtitle font sizes for better responsiveness

--- components/Title/title.js
+++ components/Title/title.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Title = ({title,subTitle}) => {
   return (
     <div className='flex justify-center flex-col items-center w-full py-10'>
-        <h1 className='text-6xl font-bold capitalize pb-2'>{title}</h1>
-        <h3 className='text-2xl  font-bold  text-center'>{subTitle}</h3>
+        <h1 className='text-2xl sm:text-6xl font-bold capitalize pb-2'>{title}</h1>
+        <h3 className='text-lg sm:text-2xl  font-bold  text-center'>{subTitle}</h3>
     </div>
   )
 }
